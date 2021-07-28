8d57e0dda858ee3079612f31732301a05fd71459
aamar22 <51492489+aamar22@users.noreply.github.com>
2021-07-29 04:27:37 +0530

    den

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Notify from './Notify'
+import Modal from './Modal'
+import Header from './header'
+import Filter from './Filter'
+import Footer from './Footer'
+
+function Layout({children}) {
+    return (
+        <div className="container">
+          
+            <NavBar />
+            
+            <Notify />
+            {/* <Modal /> */}
+            {children}
+            <Notify />
+            <Footer/>
+        </div>
+    )
+}
+
+export default Layout
---
+++ pages/about.js
@@ -0,0 +1,64 @@
+import React, { useContext } from 'react'
+import Link from 'next/link'
+import {useRouter} from 'next/router'
+
+
+function about() {
+
+                
+    return (
+        <div>
+        <div className="about">
+            <h1>ABOUT US </h1>
+            <p>We strive to have a positive impact on customers, employees, 
+                small businesses, the economy, and communities. Amazonians are smart, passionate builders with different backgrounds and goals, who share a common desire to always be learning and inventing on behalf of our customers.Although considered exotic material production in other companies, Tini Industry is working on a daily basis these types of products for the past 3 years. Our skilled technicians are familiar and comfortable with the complexities of machining exotic materials. Tini Industry team takes the mechanical properties of each material from standard to exotic prior to operations to avoid complications and to produce accurate results. For this, we have a technocratic tycoon to organize the domestic market. We commonly work with a wide range of materials and high-temp alloys including all grades of carbon, alloy, and stainless steel and the exotic metals. Our mission is to make sure customer fulfillment and confidence at all times. Our service and consistency have enabled to take pleasure in long-term and mutually beneficial relationships with our clientele. We are maintaining a constant relationship in Technical, Quality, and Production, etc, hence our clients know, our work will be done right time with the right quality.
+
+            </p>
+
+        </div>
+        <div className="team">
+            <h1>Our Team</h1>
+            <div className="teamrow">
+                <div className="team-member">
+                    <div className="team-member-img1"></div>
+                    <h2 className="h2 ct hrm dmt fwbb">Syed Anees</h2>
+                    <p className="h3 ct hrm dmbd fwb">Cheif Executive Officer</p>
+                    <div className="team-social fd">
+                        <a href=""><div className="fb-team"></div>
+                        </a>
+                        <a href=""><div className="linkin-team"></div>
+                        </a>
+                        <a href="mailto:dennis@arowdrops.in"><div className="mail-team"></div>
+                        </a>
+                    </div>
+                    <p className="p team-descrp">Having an experience of more than 28 years in the field of Fabrication of equipment with exotic material. With his extensive experience in the fabrication technology he would be contributing to the controls on Production, Planning and Control, Sub-contracting and Stores at Arowdrops.</p>
+                </div>
+                <div className="team-member">
+                    <div className="team-member-img2"></div>
+                    <h2 className="h2 ct hrm dmt fwbb">A.Dennis Thomson</h2>
+                    <p className="h3 ct hrm dmbd fwb">Chief Marketing Officer</p>
+                    <div className="team-social fd">
+                        <a href=""><div className="fb-team"></div>
+                        </a>
+                        <a href=""><div className="linkin-team"></div>
+                        </a>
+                        <a href="mailto:dennis@arowdrops.in"><div className="mail-team"></div>
+                        </a>
+                    </div>
+                    <p className="p team-descrp">Having an experience of more than 28 years in the field of Fabrication of equipment with exotic material. With his extensive experience in the fabrication technology he would be contributing to the controls on Production, Planning and Control, Sub-contracting and Stores at Arowdrops.</p>
+                </div>
+                
+            </div>
+        </div>
+        <div className="sec3">
+            <h1>How we Work</h1>
+            <p>We strive to have a positive impact on customers, employees, 
+                small businesses, the economy, and communities. Amazonians are smart, passionate builders with different backgrounds and goals, who share a common desire to always be learning and inventing on behalf of our customers.Although considered exotic material production in other companies, Tini Industry is working on a daily basis these types of products for the past 3 years. Our skilled technicians are familiar and comfortable with the complexities of machining exotic materials. Tini Industry team takes the mechanical properties of each material from standard to exotic prior to operations to avoid complications and to produce accurate results. For this, we have a technocratic tycoon to organize the domestic market. We commonly work with a wide range of materials and high-temp alloys including all grades of carbon, alloy, and stainless steel and the exotic metals. Our mission is to make sure customer fulfillment and confidence at all times. Our service and consistency have enabled to take pleasure in long-term and mutually beneficial relationships with our clientele. We are maintaining a constant relationship in Technical, Quality, and Production, etc, hence our clients know, our work will be done right time with the right quality.
+
+            </p>
+        </div>
+      </div>
+    )
+}
+
+export default about
